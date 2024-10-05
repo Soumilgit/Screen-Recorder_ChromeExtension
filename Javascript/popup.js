2d1affc53a5a4501320058d80f388b49b606d36a
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-10-05 20:28:35 +0530

    Create popup.js

---
+++ Javascript/popup.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const startVideoButton = document.querySelector("button#start_video")
+    const stopVideoButton = document.querySelector("button#stop_video")
+
+
+    startVideoButton.addEventListener("click", ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "request_recording"},  function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                } else{
+                    console.log(chrome.runtime.lastError, 'error on line 14')
+                }
+            })
+        } )
+    })
+
+
+    stopVideoButton.addEventListener("click", ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "stopvideo"},  function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                } else{
+                    console.log(chrome.runtime.lastError, 'error on line 27')
+                }
+            })
+        } )
+    })
+})
