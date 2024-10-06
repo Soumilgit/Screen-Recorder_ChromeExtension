e3d56826ad6d76c7c4992d7efe8985d7449a67b1
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-10-06 16:28:42 +0530

    Update popup.js

--- Javascript/popup.js
+++ Javascript/popup.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const startVideoButton = document.querySelector("button#start_video")
-    const stopVideoButton = document.querySelector("button#stop_video")
 
 
     startVideoButton.addEventListener("click", ()=>{
@@ -16,15 +15,5 @@ document.addEventListener("DOMContentLoaded", ()=>{
     })
 
 
-    stopVideoButton.addEventListener("click", ()=>{
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {action: "stopvideo"},  function(response){
-                if(!chrome.runtime.lastError){
-                    console.log(response)
-                } else{
-                    console.log(chrome.runtime.lastError, 'error on line 27')
-                }
-            })
-        } )
-    })
+   
 })
